6691538b225f88bb3b3df88550e3c1ba6a70e6cf
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2024-01-16 22:00:27 -0500

    Base Implementation

---
+++ ToDoList/components/AddTask.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native';
+
+const AddTask = ({ onAddTask }) => {
+    const [title, setTitle] = useState("");
+    
+    const handleAddTask = () => {
+        if (title.trim() !== "") {
+            onAddTask(title);
+            setTitle("");
+        }
+    }
+
+    return (
+        <View style = {styles.addTodoForm}>
+            <TextInput style = {styles.input} value = {title} onChangeText = {setTitle}></TextInput>
+            <Button onPress={handleAddTask}></Button>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    addTodoForm: {
+      margin: 10,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+});
+
+export default AddTask;
---
+++ ToDoList/components/ToDoList.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import {v4 as uuidv4} from 'uuid';
+import AddTask from './AddTask';
+
+const ToDoList = ({ taskTitles }) => {
+    const [toDos, setToDos] = useState(taskTitles.map((value) => ({ id: uuidv4(), title: value })));
+
+    const addToDo = (newTitle) => {
+        setToDos([...toDos, {id: uuidv4(), title: newTitle}]);
+    };
+
+    const removeToDo = (id) => {
+        setToDos(toDos.filter(item => item.id !== id));
+    }
+
+    return (
+        <View style = {styles.container}>
+        {toDos.map((item) => (
+            <View key = {item.id} style = {styles.toDoItem}>
+                <Text>{item.title}</Text>
+                <Button onPress={() => removeToDo(item.id)}></Button>
+            </View>
+        ))}
+        <AddTask onAddTask={addToDo}></AddTask>
+        </View>
+      ); 
+}
+
+ToDoList.defaultProps = {
+    taskTitles: []
+}
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+    },
+    toDoItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        marginVertical: 5,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+    }
+  });
+
+export default ToDoList;
